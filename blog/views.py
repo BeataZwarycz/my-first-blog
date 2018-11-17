22195406f412acdce29b8402a43ed8d54c2eaebe
bzwarycz <bzwarycz@interia.pl>
2018-11-17 15:51:23 +0100

    Dodałam szablon Django do pliku html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 def post_list(request):
-	#funkcja render "składa w całość" nasz szablon
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
