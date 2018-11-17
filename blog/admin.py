fdd322000d197ce95b11be87f4e296e6638e97d5
bzwarycz <bzwarycz@interia.pl>
2018-11-17 16:54:42 +0100

    Stworzyłam css do strony

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,5 @@ from .models import Post
 # Register your models here.
 
 #Chcemy, aby nasz model był widoczny w panelu admina, rejestrujemy go za pomocą poniższego polecenia:
+#admin to jest wbudowana aplikacja!!
 admin.site.register(Post)
