434af2f8ea65c5bcfff6bcd735c69704c17547c6
bzwarycz <bzwarycz@interia.pl>
2018-11-17 20:13:50 +0100

    Stworzyłam odnośniki do tytułów wpisu

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 #dodajemy wzorzec adresu URL:
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
